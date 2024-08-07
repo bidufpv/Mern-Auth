23eb3df05848de1f615a01504777db2a2bece252
bidufpv <bidufpv@gmail.com>
2024-08-07 21:37:46 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+    console.log('Connected to mongoDB');
+}).catch((error)=>{
+    console.log(`${error} connecting to mongoDB`);
+    
+})
 
 const app = express();
 const port = 3000;
 
 app.listen(port, ()=>{
     console.log(`Server is listening on ${port}` );
-    
 });
