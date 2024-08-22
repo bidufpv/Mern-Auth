81f704a84d3a8dcba94183ecdc52240a8b792587
bidufpv <bidufpv@gmail.com>
2024-08-22 15:28:45 +0530

    signin functionality added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utility/error.js";
+import jwt from 'jsonwebtoken';
 
+// Function for signup
 export const signup = async (req, res, next)=>{
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -15,6 +17,26 @@ export const signup = async (req, res, next)=>{
         
     } 
 };
+
+ 
+// Function for SignIn
+export const signin = async(req, res, next)=>{
+      const {email, password} = req.body;
+      try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User Not Found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401), 'Wrong Credentials');
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        
+const expirydate = new Date(Date.now()+3600000); //for 1hour
+
+        res.cookie('access_token', token, {httpOnly: true, expires: expirydate}).status(200).json(rest);
+      } catch (error) {
+        next(error);
+      }
+}
 
 // const deletedUser = await User.findOne({ username: 'Hero' });
     // // Delete the user with the username 'Hero'
